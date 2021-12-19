1ef2cb3afcdc5e0250b95807066d8db6c072aca6
tohhongxiang123 <tohhongxiang@gmail.com>
2021-12-20 01:24:12 +0800

    feat: Add hero section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,30 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider, NormalizeCSS, GlobalStyles } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import '../styles/globals.css'
 
 export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+	const { Component, pageProps } = props;
 
-  return (
-    <>
-      <Head>
-        <title>Seshuri</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Seshuri</title>
+				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+			</Head>
 
-      <MantineProvider
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
-      >
-        <NormalizeCSS />
-        <GlobalStyles />
-        <NotificationsProvider>
-          <Component {...pageProps} />
-        </NotificationsProvider>
-      </MantineProvider>
-    </>
-  );
+			<MantineProvider
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: 'light',
+				}}
+			>
+				<NormalizeCSS />
+				<GlobalStyles />
+				<NotificationsProvider>
+					<Component {...pageProps} />
+				</NotificationsProvider>
+			</MantineProvider>
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,32 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import React from 'react'
 
-export default function HomePage() {
-	return (
-		<>
-			<Title sx={{ fontSize: 100, fontWeight: 900, letterSpacing: -2 }} align="center" mt={100}>
-				Welcome to{' '}
-				<Text inherit variant="gradient" component="span">
-					Mantine
-				</Text>
-			</Title>
-			<Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-				This starter Next.js projects includes a minimal setup for server side rendering, if you
-				want to learn more on Mantine + Next.js integration follow{' '}
-				<Anchor href="https://mantine.dev/theming/next/" size="lg">
-					this guide
-				</Anchor>
-				. To get started edit index.tsx file.
-			</Text>
-		</>
-	);
+export default function index() {
+    return (
+        <div>
+            <nav className='p-8 flex justify-around bg-black text-white'>
+                <p className="text-xl font-bold tracking-wider">Seshuri</p>
+                <ul className="flex gap-x-8">
+                    <li><p>About</p></li>
+                    <li><p>Features</p></li>
+                    <li><p>Client Portal</p></li>
+                </ul>
+            </nav>
+            <section className='bg-gradient-to-br from-blue-700 to-cyan-300 pt-16'>
+                <div className='md:mx-16'>
+                    <div className='bg-bottom lg:bg-right bg-cover md:bg-contain bg-no-repeat h-screen flex justify-center lg:justify-start items-start lg:items-center' style={{ backgroundImage: 'url("/img-top.png")' }}>
+                        <div className='text-gray-100 text-center text-shadow lg:ml-16 xl:ml-40 lg:-mt-96 xl:-mt-72'>
+                            <h1 className='text-8xl font-bold mb-4'>Seshuri</h1>
+                            <p className='text-3xl font-semibold opacity-90 mb-8'>Presence, reimagined.</p>
+                            <button className='bg-black text-white px-8 py-4 rounded-md font-semibold uppercase tracking-wide font-mono hover:shadow-md hover:-translate-y-0.5 transition-all duration-75'>{'>'} Let's talk</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="pt-16 flex flex-col justify-center items-center gap-8 bg-gray-800 text-gray-200">
+                <p className="font-thin tracking-widest uppercase opacity-70">Seshuri by Laminar Tech</p>
+                <h2 className="text-4xl font-bold tracking-wide max-w-xl text-center">The on-demand website team that you never had to hire</h2>
+                <img src="dash-device.png" className="max-w-2xl" />
+            </section>
+        </div>
+    )
 }
